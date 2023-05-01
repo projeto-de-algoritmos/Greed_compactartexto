5f8c0be1b6c87f3a96269b6d977037ed779e06f4
Weslley Barros <weslley17w@gmail.com>
2023-05-01 13:04:38 -0300

    Initial commit
    Co-authored-by: oo7gabriel <gabrielfsacademico@gmail.com>

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+  import Header from './components/header.vue'
+  import Body from './components/body.vue'
+</script>
+
+<style>
+.vue-select{
+  margin: 10px !important;
+}
+.vue-dropdown, .vue-input input{
+  color: #000000 !important;
+}
+  .vue-select-header{
+      height: 40px !important;
+      background: #ffffff !important;
+      
+    }
+</style>
+<template>
+  <Header msg="Árvore Genealógica" />
+  <Body />
+</template>
---
+++ src/components/header.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+   <header>
+    <a>{{ msg }}</a>
+  </header>
+</template>
+
+<style scoped>
+header {
+  min-height: 50px;
+  min-width: 100%;
+  background-color: var(--color-heading);
+  text-align: center;
+  padding: 12px;
+}
+
+a,
+.green {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s;
+  font-size: 1.5rem;
+  margin-top: 60px;
+  
+}
+
+@media (hover: hover) {
+  a:hover {
+    color: hsla(160, 100%, 37%, 0.7);
+  }
+}
+</style>
